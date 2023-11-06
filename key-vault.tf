9e229237f186ab649ef3f16aecf539fd411dcf1f
Ash Dey <ashabc [at] hotmail.com>
2023-11-06 17:51:16 +1100

    defender report v0.1

--- key-vault.tf
+++ key-vault.tf
@@ -1,3 +1,5 @@
+
+/*
 resource "azurerm_key_vault_access_policy" "keyvault_policy_cms" {
   key_vault_id = data.azurerm_key_vault.vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
@@ -6,6 +8,16 @@ resource "azurerm_key_vault_access_policy" "keyvault_policy_cms" {
     "Get"
   ]
 }
+
+*/
+
+resource "azurerm_role_assignment" "kv" {
+  scope                = data.azurerm_key_vault.vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_function_app.fa.identity[0].principal_id
+  depends_on           = [ azurerm_linux_function_app.fa ]
+}
+
 
 data "azurerm_key_vault_secret" "azure-tenant-id" {
   name         = "azure-tenant-id"
