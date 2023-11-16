e54e0557aa097108f6d05a9a9b11e949b8e2d300
Ash Dey <ashabc [at] hotmail.com>
2023-11-16 16:09:41 +1100

    add private endpoint for function

--- main.tf
+++ main.tf
@@ -54,12 +54,12 @@ resource "azurerm_linux_function_app" "fa" {
 
   app_settings = {
 
-    azure_tenant_id          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.azure_tenant_id.versionless_id})"
-    defender_app_id          = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.defender_app_id.versionless_id})"
-    defender_app_secret      = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.defender_app_secret.versionless_id})"
-    sumo_collector_url       = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sumo_collector_url.versionless_id})"
-    comm_service_conn_string = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.comm_service_conn_string.versionless_id})"
-    WEBSITE_TIME_ZONE        = "AU"
+    azure_tenant_id                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.azure_tenant_id.versionless_id})"
+    defender_app_id                     = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.defender_app_id.versionless_id})"
+    defender_app_secret                 = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.defender_app_secret.versionless_id})"
+    sumo_collector_url                  = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.sumo_collector_url.versionless_id})"
+    comm_service_conn_string            = "@Microsoft.KeyVault(SecretUri=${data.azurerm_key_vault_secret.comm_service_conn_string.versionless_id})"
+    WEBSITE_TIME_ZONE                   = "AU"
     WEBSITES_ENABLE_APP_SERVICE_STORAGE = true
 
   }
